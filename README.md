816176982c6a83719c710fe17cbd0893c52d0be0
Marco <marcobert37@gmail.com>
2023-12-03 11:38:23 +0100

    feat(greed)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Jac-Basi-Javascript-03
 Correction of lesson 3 exercises
+
+# Greed Dice Game
+Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
---
+++ greed.js
@@ -0,0 +1,37 @@
+const _ = require("lodash");
+
+function score(dice) {
+    const counts = _.groupBy(dice);
+  
+    let score = 0;
+  
+    _.forEach(counts, (count, value) => {
+      const val = parseInt(value, 10);
+      const tripletCount = Math.floor(count.length / 3);
+      const remainder = count.length % 3;
+  
+      if (val === 1) {
+        score += tripletCount * 1000 + remainder * 100;
+      } else if (val === 5) {
+        score += tripletCount * 500 + remainder * 50;
+      } else {
+        if (tripletCount > 0) {
+          score += val * 100 * tripletCount;
+        }
+      }
+    });
+  
+    return score;
+  }
+
+// Example usage:
+const diceValues = [1, 1, 1, 5, 1];
+const totalScore = score(diceValues);
+
+console.log("Total score:", totalScore);
+
+// Example with text score:
+const secondDiceValues = [1, 1, 1, 3, 1];
+const secondTotalScore = score(secondDiceValues);
+
+console.log("Total score 2:", secondTotalScore);
